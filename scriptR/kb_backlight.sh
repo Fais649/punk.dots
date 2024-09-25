276a191374f121b72d7bf295bfe37ae6da24dd99
Faisal Alalaiwat <faisalalalaiwat@MacBook-Pro-von-Faisal.local>
2024-09-25 14:41:45 +0200

    fixups

--- scriptR/kb_backlight.sh
+++
@@ -1,9 +0,0 @@
-BRIGHTNESS=$(echo dead | sudo -S cat /sys/class/leds/asus\:\:kbd_backlight/brightness)
-
-if [[ $BRIGHTNESS -eq 0 ]]; then
-	BRIGHTNESS=1
-else
-	BRIGHTNESS=0
-fi
-
-echo $BRIGHTNESS > /sys/class/leds/asus\:\:kbd_backlight/brightness
